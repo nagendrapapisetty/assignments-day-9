0226366c831d3e06960fd1b31c413db5ac48675a
unknown <papisettynagendra>
2022-08-14 08:54:43 +0530

    My First Message

---
+++ EmployeeWages.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeewages;
+
+public class EmployeeWages {
+    public static void main(String[] args) {
+        System.out.println("Welocome To EmployeeWages");
+    }
+}
